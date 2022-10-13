3fe186ebbb27dfd22d210b59904f0892b7ac1db8
Amarjothi Manigandan <MANIAM@ladmins-MacBook-Pro.local>
2022-10-13 15:30:21 +0100

    updated

---
+++ src/main/java/works/weave/socks/cart/configuration/UserConfiguration.java
@@ -0,0 +1,24 @@
+package works.weave.socks.cart.configuration;
+import io.featurehub.client.ClientContext;
+import io.featurehub.client.FeatureHubConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Configuration
+public class UserConfiguration {
+  @Bean
+  @Scope("request")
+  ClientContext featureHubClient(FeatureHubConfig fhConfig, HttpServletRequest request) {
+    ClientContext fhClient = fhConfig.newContext();
+
+    if (request.getHeader("Authorization") != null) {
+      // you would always authenticate some other way, this is just an example
+      fhClient.userKey(request.getHeader("Authorization"));
+    }
+
+    return fhClient;
+  }
+}
