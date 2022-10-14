ca5dcfff0e5f78ff0131c5981d16a834405b0f47
Amarjothi Manigandan <MANIAM@ladmins-MacBook-Pro.local>
2022-10-14 09:41:37 +0100

    updated

--- src/main/java/works/weave/socks/cart/CartApplication.java
+++ src/main/java/works/weave/socks/cart/CartApplication.java
@@ -4,10 +4,34 @@ import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.featurehub.client.ClientContext;
+import io.featurehub.client.EdgeFeatureHubConfig;
+import io.featurehub.client.FeatureHubConfig;
+
 @SpringBootApplication
 @EnablePrometheusEndpoint
 public class CartApplication {
     public static void main(String[] args) {
         SpringApplication.run(CartApplication.class, args);
     }
+
+      @Bean
+  public FeatureHubConfig featureHubConfig() {
+    String host = System.getenv("FEATUREHUB_EDGE_URL");
+
+    if (host == null) {
+      throw new RuntimeException("Unable to determine the host for FeatureHub");
+    }
+
+    String apiKey = System.getenv("FEATUREHUB_API_KEY");
+
+    if (apiKey == null) {
+      throw new RuntimeException("Unable to determine the API key for FeatureHub");
+    }
+
+    FeatureHubConfig config = new EdgeFeatureHubConfig(host, apiKey);
+    config.init();
+
+    return config;
+  }
 }
